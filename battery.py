9557a16ae4da18bb4012c7b512c40c073ccf691e
Zsolt Kebel <25386639+zsoltkebel@users.noreply.github.com>
2024-11-27 11:47:31 +0000

    Add going to charger on route

--- battery.py
+++ battery.py
@@ -1,5 +1,8 @@
+from pyrobosim.core import Robot, World, ObjectSpawn, Location
 from pyrobosim.utils.pose import Pose
-from pyrobosim.utils.path import Path
+from pyrobosim.utils.motion import Path
+from pyrobosim.navigation import ConstantVelocityExecutor, RRTPlanner
+
 import time
 
 class ChargingStation:
@@ -69,14 +72,69 @@ class Battery:
         workingCharge = self.getCharge()
         currentPose = startingPose
         stepCount = 0
-        while (stepCount<len(path) and workingCharge >= 0):
-            workingCharge -= currentPose.get_linear_distance(path[stepCount]) * self.drainPerDistanceUnit
-            workingCharge -= abs(currentPose.get_angular_distance(path[stepCount]) * self.drainPerRadianRotate)
-            stepCount += 1
+        while (stepCount<len(path)-1 and workingCharge >= 0):
             currentPose = path[stepCount]
+            workingCharge -= currentPose.get_linear_distance(path[stepCount + 1]) * self.drainPerDistanceUnit
+            workingCharge -= abs(currentPose.get_angular_distance(path[stepCount + 1]) * self.drainPerRadianRotate)
+            stepCount += 1
+            print("wcharge:", workingCharge)
         if (actions and len(actions)>0):
             for action in actions:
                 workingCharge -= self.actionDrains[action]
         self.charge = workingCharge
+
+    def get_optimal_charger(self, start: Pose, world: World, planner: RRTPlanner, goal: Pose = None) -> tuple[Path, Location]:
+        """Returns the charger that adds the least amount of extra distance to visit."""
+        chargers: list[Location] = world.get_locations(["charger"])
+
+        path_length = None
+        best_charger = None
+        best_charger_pose = None
+        path_to_charger = None
+
+        for charger in chargers:
+            for docking_pose in charger.nav_poses:
+                plan_to_charger = planner.plan(start, docking_pose)
+                
+                if plan_to_charger.num_poses == 0:
+                    print("couldnt find oath to charger", charger, docking_pose)
+                    continue
+
+                plan_to_goal = None
+                if goal is not None:
+                    plan_to_goal = planner.plan(docking_pose, goal)
+                    
+                    length = plan_to_charger.length + plan_to_goal.length
+                else:
+                    length = plan_to_charger.length
+
+                if path_length is None or length < path_length:
+                    path_length = length
+                    best_charger = charger
+                    best_charger_pose = docking_pose
+                    path_to_charger = plan_to_charger
+
+        return (path_to_charger, best_charger)
+
+    def can_complete_path(self, path: Path, with_charge: float=None) -> bool:
+        """Returns whther battery capacity is enough to complete path."""
+        charge = with_charge
+        if charge is None:
+            charge = self.charge
+        return charge >= self.get_drain_for_path(path)
+
+    def get_drain_for_path(self, path: Path) -> float:
+        """Return the battery units needed to complete the given path."""
+        drain = 0.0
+        for i in range(path.num_poses - 1):
+            current_pose = path.poses[i]
+            next_pose = path.poses[i + 1]
+
+            linear_drain = current_pose.get_linear_distance(next_pose) * self.drainPerDistanceUnit
+            angular_drain = abs(current_pose.get_angular_distance(next_pose) * self.drainPerRadianRotate)
+
+            drain += linear_drain + angular_drain
+        # left actions out for now  
+        return drain
         
     
--- fossil_collector_robot.py
+++ fossil_collector_robot.py
@@ -28,7 +28,7 @@ from pyrobosim_msgs.srv import RequestWorldState
 from pyrobosim.planning.actions import TaskAction, TaskPlan
 
 from fossil_world_generation import create_fossil_world
-
+from battery import Battery
 
 class FossilException(Exception):
     """smtg"""
@@ -64,6 +64,11 @@ class CollectorRobot(WorldROSWrapper):
 
         self.first = True
 
+        # battery
+        self.battery = Battery(100.0, [], 2.0, 1.0, {})
+
+        self.on_charge_finished_path = None
+
     def get_robot(self) -> Robot:
         collector: Robot = self.world.get_robot_by_name(COLLECTOR)
         return collector
@@ -78,18 +83,37 @@ class CollectorRobot(WorldROSWrapper):
                     return True
         return False
 
-    def battery_behavior(self, charge_rate: float=30.0, drain_rate: float=10.0):
+    def battery_behavior(self, charge_rate: float=10.0, drain_rate: float=10.0):
         robot = self.get_robot()
 
         if self.is_at_charger():
-            robot.battery_level = min(robot.battery_level + charge_rate, 100.0)
-        elif robot.is_moving():
-            robot.battery_level = max(robot.battery_level - drain_rate, 0.0)
+            print("charging...")
+            robot.battery_level = min(self.battery.charge + charge_rate, 100.0)
+            self.battery.charge = min(self.battery.charge + charge_rate, 100.0)
+
+            if self.battery.charge == 100.0 and self.on_charge_finished_path is not None:
+                result = robot.follow_path(self.on_charge_finished_path)
+                self.on_charge_finished_path = None
+
+                robot.pick_object("fossil")
+
+        # object = world.get_object_by_name("fossil1")
+        # spawn: ObjectSpawn = object.parent
+        # for pose in spawn.nav_poses:
+        #     print("fossil1", pose.x, pose.y, pose.z, pose.q)
+
+
+        # elif robot.is_moving():
+        #     robot.battery_level = max(self.battery.charge - drain_rate, 0.0)
+        #     self.battery.charge = min(self.battery.charge - drain_rate, 0.0)
         
         world.gui.on_robot_changed()
-
+    
     def fossil_discovery_callback(self, msg):
-        """Handle new messages on fossil discovery topic."""
+        """Handle new messages on fossil discovery topic.
+        
+        accept messages of form {"name": "string"}
+        """
         try:
             data = json.loads(msg.data)
             self.get_logger().info(f"Recieved fossil data:\n{data}")
@@ -102,7 +126,15 @@ class CollectorRobot(WorldROSWrapper):
             # parent: Location = world.get_object_by_name(data["name"]).parent
             # g = parent.nav_poses[0]
             # self.go_to_pose(g)
-            self.execute_collect_fossil_plan(data["name"])
+
+            # self.execute_collect_fossil_plan(data["name"])
+            try:
+                # self.go_to_fossil_safely(data["name"])
+
+                g = Pose(x=-9.457152293593143, y=-6.258005503665991, z=0.00, q=[1.0, 0.0, 0.0, 0.0])
+                self.go_to_pose_through_charger(g)
+            except Exception as error:
+                self.get_logger().warn("Warning")
 
         if msg.data.startswith('FOSSIL_FOUND:'):
             x, y = map(float, msg.data.split(':')[1].split(','))
@@ -171,7 +203,9 @@ class CollectorRobot(WorldROSWrapper):
         """Return path planned by robot's RRTPlanner."""
         planner: RRTPlanner = self.get_robot().path_planner
         start = self.get_robot().get_pose()
+        print("planning", start, goal_pose)
         plan = planner.plan(start, goal_pose)
+        print("plan", plan)
         return plan
 
     def execute_collect_fossil_plan(self, fossil_obj_name: str):
@@ -188,6 +222,74 @@ class CollectorRobot(WorldROSWrapper):
 
         self.get_logger().info(f"result: {num_completed}, {result.message}")
 
+        planner: RRTPlanner = robot.path_planner
+        print("latest path poses:", planner.latest_path.poses)
+        self.battery.completeDrainForPath(planner.latest_path.poses, planner.latest_path.poses[0])
+        print("drain result:", self.battery.charge)
+
+    def go_to_fossil_safely(self, fossil_name):
+        query_list = [elem for elem in fossil_name.split(" ") if elem]
+        goal = query_to_entity(
+            self.world,
+            query_list,
+            mode="location",
+            robot=self.get_robot(),
+            resolution_strategy="nearest",
+        )
+        goal_node = self.world.graph_node_from_entity(goal, robot=self.get_robot())
+
+        # self.go_to_pose_safely(goal_node.pose)
+        self.go_to_pose_through_charger(goal_node.pose)
+    
+    def go_to_pose_through_charger(self, goal: Pose):
+        path = self.get_plan_to_pose(goal)
+        robot = world.get_robot_by_name(COLLECTOR)
+
+        self.battery.charge = 50.0
+
+        if False and self.is_safe_path(path):
+            result = robot.follow_path(path)
+        else:
+            # go to charger on the way
+            planner: RRTPlanner = robot.path_planner
+            path_to_charger, charger_location = self.battery.get_optimal_charger(robot.get_pose(), world, planner, goal=goal)
+            print("optimal charger:", charger_location)
+
+            result = robot.follow_path(path_to_charger)
+
+            self.on_charge_finished_path = planner.plan(path_to_charger.poses[path_to_charger.num_poses - 1], goal)
+
+            # result = robot.follow_path(path_from_charger_to_goal)
+
+    def go_to_pose_safely(self, goal: Pose):
+        path = self.get_plan_to_pose(goal)
+        robot = world.get_robot_by_name(COLLECTOR)
+
+
+        if self.is_safe_path(path):
+            result = robot.follow_path(path)
+        else:
+            # go to charger on the way
+            planner: RRTPlanner = robot.path_planner
+            path_to_charger, charger_location = self.battery.get_optimal_charger(robot.get_pose(), world, planner, goal=goal)
+
+            result = robot.follow_path(path_to_charger)
+
+            path_from_charger_to_goal = planner.plan(path_to_charger.poses[path_to_charger.num_poses - 1], goal)
+
+            result = robot.follow_path(path_to_charger)
+
+
+    def is_safe_path(self, path: Path) -> bool:
+        """Returns whther the robot can complete path and then go to a charger worst case scenario."""
+        robot = world.get_robot_by_name(COLLECTOR)
+        goal_pose = path.poses[path.num_poses - 1]
+        path_to_charger, charger_location = self.battery.get_optimal_charger(goal_pose, world, robot.path_planner)
+
+        drain_path = self.battery.get_drain_for_path(path)
+        drain_charger_path = self.battery.get_drain_for_path(path_to_charger)
+
+        return self.battery.charge >= drain_path + drain_charger_path
 
 if __name__ == "__main__":
     rclpy.init()
